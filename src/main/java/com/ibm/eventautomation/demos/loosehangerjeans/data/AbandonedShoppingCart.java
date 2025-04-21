f9c08ba707faf843bf896bb31bd9598dc1c31d16
Ami Thomas <Ami.Thomas@ibm.com>
2025-04-21 21:49:19 +0530

    feat: define shape of events
    define shape of events for abandoned cart
    
    Contributes to: https://jsw.ibm.com/browse/EVI-11776
    
    Signed-off-by: Ami Thomas <Ami.Thomas@ibm.com>

---
+++ src/main/java/com/ibm/eventautomation/demos/loosehangerjeans/data/AbandonedShoppingCart.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright 2025 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.ibm.eventautomation.demos.loosehangerjeans.data;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Represents an event for an abandoned shopping cart.
+ * Each event includes customer information and a list of products
+ * left in the cart when it was abandoned.
+ */
+
+public class AbandonedShoppingCart extends LoosehangerData {
+
+    public static final String PARTITION = "abandonedshoppingcart";
+
+    /** Unique ID for the abandoned cart */
+    private final String cartId;
+
+    /** Time that the cart was considered abandoned */
+    private final String abandonTime;
+
+    /** Details of the customer who made the order. */
+    private final OnlineCustomer customer;
+
+    /** List of products left in the cart */
+    private final List<Product> products;
+
+    /** Schema for the events - all fields are required. */
+    private static final Schema SCHEMA = SchemaBuilder.struct()
+            .name("abandonedshoppingcart")
+            .version(1)
+            .field("cartId",      Schema.STRING_SCHEMA)
+            .field("customer",    OnlineCustomer.SCHEMA)
+            .field("products",    SchemaBuilder.array(Schema.STRING_SCHEMA).build())
+            .field("abandonTime", Schema.STRING_SCHEMA)
+            .build();
+
+    /** Creates an {@link AbandonedShoppingCart} using the provided details */
+    public AbandonedShoppingCart(String cartId, String abandonTime, OnlineCustomer customer, List<Product> products, ZonedDateTime recordTimestamp) {
+        super(recordTimestamp);
+        this.cartId = cartId;
+        this.abandonTime = abandonTime;
+        this.customer = customer;
+        this.products = products;
+    }
+
+    /** Creates an {@link OnlineOrder} using the provided details.
+     * The ID is generated randomly.
+     * */
+    public AbandonedShoppingCart(String abandonTime, OnlineCustomer customer, List<Product> products, ZonedDateTime recordTimestamp) {
+        this(UUID.randomUUID().toString(), abandonTime, customer, products, recordTimestamp);
+    }
+
+    public String getAbandonTime() {
+        return abandonTime;
+    }
+
+    public String getCartId() {
+        return cartId;
+    }
+
+    public String getTimestamp() {
+        return abandonTime;
+    }
+
+    public OnlineCustomer getCustomer() {
+        return customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public SourceRecord createSourceRecord(String topicName) {
+        return super.createSourceRecord(topicName, PARTITION);
+    }
+
+    @Override
+    protected String getKey() {
+        return cartId;
+    }
+
+    @Override
+    protected Schema getValueSchema() {
+        return SCHEMA;
+    }
+
+    @Override
+    protected Struct getValue() {
+        Struct struct = new Struct(SCHEMA);
+        struct.put(SCHEMA.field("id"),          cartId);
+        struct.put(SCHEMA.field("customer"),    customer.toStruct());
+        struct.put(SCHEMA.field("products"),    products);
+        struct.put(SCHEMA.field("abandonTime"),   abandonTime);
+        return struct;
+    }
+
+    @Override
+    public String toString() {
+        return "AbandonedShoppingCart [cartId=" + cartId + ", abandonTime=" + abandonTime + ", customer=" + customer + ", products="
+                + Arrays.toString(products.toArray()) + "]";
+    }
+}
